54b99a12f79dccad12db4a7bf426edb8fbbb5c94
venidium <venidium@gmail.com>
2023-02-27 11:25:18 +0900

    first commitsecond

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,12 +2,12 @@ import Link from 'next/link';
 
  function Header() {
      return <header>
-             <h1>ねくすと・じぇいえす事始め!</h1>
+             <h1>ねくすと・じぇいえす事始め!20230227</h1>
              <menu>Menu：
                  <Link href="/">トップ</Link>｜
                  <Link href="/other">別ページ</Link>
              </menu>
        </header>;
    }
-
+ 
    export default Header;
